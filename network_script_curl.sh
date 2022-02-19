282cac6002f23d3c14b863662e4ff1df426faefa
bhagwat-99 <shindebhagavat030@gmail.com>
2022-02-19 18:06:13 +0530

    update

--- network_script_curl.sh
+++ network_script_curl.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#check for root
-UID=$(id -u)
-if [ x$UID != x0 ]
-then
-    #Beware of how you compose the command
-    printf -v cmd_str '%q ' "$0" "$@"
-    exec echo 'torizoncore' | sudo -S echo && sudo su -c "$cmd_str"
-fi
-
-#I am root
-#and the rest of your commands
-
-#echo 'torizoncore' | sudo -S echo && sudo su
-#echo 'torizoncore' | sudo -S su && sudo su
-
-#echo 'torizoncore' | sudo -S chown root:gpio /sys/class/gpio/unexport /sys/class/gpio/export
 
 # exporting the gpio4.IO01 - 480 - 32 * 4 + 1 = 353
 echo 353 > /sys/class/gpio/export
@@ -31,7 +15,7 @@ echo 0 > /sys/class/gpio/gpio353/value
 led_state=OFF
 while true
 do
-  	network_status=$(nmcli -t -f STATE g)
+        network_status=$(nmcli -t -f STATE g)
 
         if [[ $network_status = connected ]]
         then
@@ -40,14 +24,17 @@ do
                 then
                     	if [[ $led_state = OFF ]]
                         then
-
                             	led_state=ON
                                 echo 1 > /sys/class/gpio/gpio353/value
                                 echo "connected to network"
-                        else echo "connected to network"
                         fi
-                else echo "Connected but no internet"
-                echo 0 > /sys/class/gpio/gpio353/value
+                else
+                        echo "Connected but no internet"
+                        echo 0 > /sys/class/gpio/gpio353/value
                 fi
+        else
+                led_state=OFF
+                echo 0 > /sys/class/gpio/gpio353/value
+                echo "Not Connected"
         fi
 done
--- network_script_ping.sh
+++ network_script_ping.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-#check for root
-UID=$(id -u)
-if [ x$UID != x0 ]
-then
-    #Beware of how you compose the command
-    printf -v cmd_str '%q ' "$0" "$@"
-    exec echo 'torizoncore' | sudo -S echo && sudo su -c "$cmd_str"
-fi
-
-#I am root
-#and the rest of your commands
-
-#echo 'torizoncore' | sudo -S echo && sudo su
-#echo 'torizoncore' | sudo -S su && sudo su
-
-#echo 'torizoncore' | sudo -S chown root:gpio /sys/class/gpio/unexport /sys/class/gpio/export
 
 # exporting the gpio4.IO01 - 480 - 32 * 4 + 1 = 353
 echo 353 > /sys/class/gpio/export
@@ -44,10 +28,10 @@ do
                                 led_state=ON
                                 echo 1 > /sys/class/gpio/gpio353/value
                                 echo "connected to network"
-                        else echo "connected to network"
                         fi
-                else echo "Connected but no internet"
-                echo 0 > /sys/class/gpio/gpio353/value
+                else
+                        echo "Connected but no internet"
+                        echo 0 > /sys/class/gpio/gpio353/value
 
                 fi
 
