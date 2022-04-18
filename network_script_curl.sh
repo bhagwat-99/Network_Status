6316cdea25f9aab1594dc1416a8f97f9ad6ed98e
bhagwat-99 <shindebhagavat030@gmail.com>
2022-04-18 17:30:40 +0530

    curl command

---
+++ network_script_curl.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#echo 353 > /sys/class/gpio/export
+
+#echo "out" > /sys/class/gpio/gpio353/direction
+#echo 0 > /sys/class/gpio/gpio353/value
+
+led_state=OFF
+while true
+do
+    internet_status=$(curl -sI http://www.google.com | awk '/200/ {print $2}')
+
+    if [[ $internet_status = 200 ]]
+    then
+        if [ $led_state = OFF ]
+        then
+            led_state=ON
+            #echo 1 > /sys/class/gpio/gpio353/value
+            echo "connected"
+        fi
+    else
+        #echo 0 > /sys/class/gpio/gpio353/value
+        echo "disconnected"
+        led_state=OFF
+    fi
+done
